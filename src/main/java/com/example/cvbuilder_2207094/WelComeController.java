b39897f71734e083e69194a75963764e47d5d663
Rana <ranaislam135790@gmail.com>
2025-11-17 18:39:54 +0600

    Welcome page fxml created Successfully

---
+++ src/main/java/com/example/cvbuilder_2207094/WelComeController.java
@@ -0,0 +1,17 @@
+package com.example.cvbuilder_2207094;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class WelComeController {
+
+    @FXML
+    private Button next;
+
+    @FXML
+    void GoNext(MouseEvent event) {
+
+    }
+
+}
