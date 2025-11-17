6ab565d683945c683359f20901a3eb04879ab401
Rana <ranaislam135790@gmail.com>
2025-11-17 23:08:34 +0600

    CV page controller defination

--- src/main/java/com/example/cvbuilder_2207094/CvPageController.java
+++ src/main/java/com/example/cvbuilder_2207094/CvPageController.java
@@ -1,6 +1,60 @@
 package com.example.cvbuilder_2207094;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class CvPageController {
 
-}
+    @FXML
+    private TextArea address;
 
+    @FXML
+    private TextArea boardtxt;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextArea examtxt;
+
+    @FXML
+    private TextArea institutetxt;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField phone;
+
+    @FXML
+    private TextArea projecttxt;
+
+    @FXML
+    private TextArea resulttxt;
+
+    @FXML
+    private TextArea sessiontxt;
+
+    @FXML
+    private TextArea skilltxt;
+
+    @FXML
+    private Button submitCv;
+
+    @FXML
+    private TextArea worktxt;
+
+    @FXML
+    void submit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void submitCvNext(MouseEvent event) {
+
+    }
+
+}
